2b5cd5a0d2fc47bc2c953d4efbcc425252a4d9ad
KaueBalani <kauebalani821@outlook.com>
2025-09-15 15:59:09 -0300

    parte 4 e 5

---
+++ includes/topo.php
@@ -0,0 +1,20 @@
+<?php
+    session_start();
+?>
+
+<div class="card">
+    <div class="card-header">
+        <h1> Projeto Blog em PHP + MySql IFSP - KaueBalani </h1>
+    </div>
+    <?php
+        if (isset($_SESSION['login'])):
+    ?>
+    <div class="card-body text-right">
+            Olá <?php echo $_SESSION['login']['usuario']['nome'] ?>!
+    </div>
+    <a href="core/usuario_repositorio.php?acao=logout"
+        class="btn btn-link btn-sm" role="button">Sair</a>
+</div>
+<?php
+    endif
+?>
---
+++ pagina_teste.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Post | Projeto para web com php</title>
+        <link rel="stylesheet" href="lib/bootstrap-4.2.1-dist/css/bootstrap.min.css">
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <?php
+                        include 'includes/topo.php';
+                    ?>
+                </div>
+            </div>
+            <div class="row" style="min-height: 500px;">
+                <div class="col-md-12">
+                    <?php include 'includes/rodape.php'; ?>
+                </div>
+            </div>
+        </div>
+        <script scr="lib/bootstrap-4.2.1-dist/js/bootstrap.min.js"></script>
+    </body>
+</html>
